038bad47473cb9ba5d055aa3b8cb187f7c3edf63
Kimbyul88 <byul88byul@gmail.com>
2024-07-19 20:28:17 +0900

    First Commit

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/home/home";
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import "../../styles/color.css";
+const Home = () => {
+  return <Wrapper>home</Wrapper>;
+};
+
+export default Home;
+
+const Wrapper = styled.div`
+  background-color: var(--red-600);
+  height: 100vh;
+`;
