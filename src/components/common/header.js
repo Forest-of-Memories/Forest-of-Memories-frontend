0a9b8c019a297a4081dd8c0ffe75b94afd991c73
Kimbyul88 <byul88byul@gmail.com>
2024-07-22 19:43:05 +0900

    pull 전에 commit

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import "../../styles/color.css";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  // const location = useLocation();
   return <Wrapper>header</Wrapper>;
 };
 
