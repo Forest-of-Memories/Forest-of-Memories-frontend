c1b3e21f900de3b1a243316911530b27e2cd7e73
Saereem Lee <reems0815@gmail.com>
2024-08-02 01:24:24 +0900

    modify question detail page & mypage

--- src/pages/mypage/mypage-setting.jsx
+++ src/pages/mypage/mypage-setting.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Footer from "../../components/common/footer";
+
 import My from "../../components/mypage/MyPageComponent";
 
 const MyPage = () => {
   return (
     <div>
       <My />
-      <Footer />
     </div>
   );
 };
